a1ec9304a9a1e2ed71613e9a4243c210eb8457b7
Diptanshu123 <88224790+Diptanshu123@users.noreply.github.com>
2021-08-01 23:20:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function laptop(){
+    window.location= "laptop.html";
+}
+function phone(){
+    window.location = "phone.html";
+}
+function tab(){
+    window.location = "tab.html";
+}
+function traffic(){
+    window.location = "traffic.html";
+}
